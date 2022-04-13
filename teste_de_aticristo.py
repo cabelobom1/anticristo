afa2f84666410364f342bb1f649edd30ca319ce6
cabelobom1 <102665922+cabelobom1@users.noreply.github.com>
2022-04-13 08:57:35 -0300

    Create teste_de_aticristo.py
    manda sua religião

---
+++ teste_de_aticristo.py
@@ -0,0 +1,10 @@
+religiao=input("religiao dele: católico, evangélico, satanico e ateu.")
+print(religiao)
+if religiao=="católico":
+ print("gosta de Jesus")
+elif religiao=="evangélico":
+  print("gosta de jesus")
+elif religiao=="santanico":
+  print("não de Dele. anticristo")
+elif religiao=="ateu":  
+  print("um dia acredita")
